12156f87c1dda68befdadec06464bfc92cbb1c17
raowenfeng <raowenfeng@ama-auto.com>
2026-05-07 18:15:53 +0800

    fix bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "MarkDown to WeChat",
-  description: "",
+  description: 'Convert Markdown into styled WeChat public account content with live preview and copy functionality.'
 };
 
 export default function RootLayout({
